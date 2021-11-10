90131a8ac64c9ea17d9b4f95416bfbce937ea9de
OmarJ <omarjandali93@gmail.com>
2021-11-10 14:18:27 -0800

    added the fruits and got them to render and start moving on the x and y acis

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import pygame
+from random import randint
+
 pygame.init()
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode([500, 500])
 
 class GameObject(pygame.sprite.Sprite):
@@ -11,16 +14,48 @@ class GameObject(pygame.sprite.Sprite):
   
   def render(self, screen):
     screen.blit(self.surf, (self.x, self.y))
-    
-apple = GameObject(100, 100, 'apple.png')
-apple = GameObject(100, 250, 'strawberry.png')
-apple = GameObject(100, 400, 'apple.png')
-apple = GameObject(250, 100, 'strawberry.png')
-apple = GameObject(250, 250, 'apple.png')
-apple = GameObject(250, 400, 'strawberry.png')
-apple = GameObject(400, 100, 'apple.png')
-apple = GameObject(400, 250, 'strawberry.png')
-apple = GameObject(400, 400, 'apple.png')
+
+class Apple(GameObject):
+  def __init__(self):
+    super(Apple, self).__init__(0, 0, 'apple.png')
+    self.dx = 0
+    self.dy = (randint(0, 200) / 100) + 1
+    self.reset()
+
+  def move(self):
+    self.x += self.dx
+    self.y += self.dy
+    if self.y > 500: 
+      self.reset()
+
+  def reset(self):
+    self.x = randint(50, 400)
+    self.y = -64
+
+class Strawberry(GameObject):
+  def __init__(self):
+    super(Strawberry, self).__init__(0, 0, 'strawberry.png')
+    self.dx = (randint(0, 200) / 100) + 1
+    self.dy = 0
+    self.reset()
+
+  def move(self):
+    self.x += self.dx
+    self.y += self.dy
+    if self.x > 500: 
+      self.reset()
+
+  def reset(self):
+    self.x = -64
+    self.y = randint(50, 400)
+
+apple1 = Apple()
+apple2 = Apple()
+apple3 = Apple()
+
+strawberry1 = Strawberry()
+strawberry2 = Strawberry()
+strawberry3 = Strawberry()
 
 running = True
 while running:
@@ -29,6 +64,20 @@ while running:
     if event.type == pygame.QUIT:
       running = False
   screen.fill((255, 255, 255))
-  apple.render(screen)
+
+  apple1.move()
+  apple2.move()
+  apple1.move()
+  strawberry1.move()
+  strawberry2.move()
+  strawberry3.move()
+
+  apple1.render(screen)
+  apple2.render(screen)
+  apple3.render(screen)
+  strawberry1.render(screen)
+  strawberry2.render(screen)
+  strawberry3.render(screen)
+
   pygame.display.flip()
-  
+  clock.tick(60)
